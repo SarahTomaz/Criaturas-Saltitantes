a14bb6c78154be7217e704882efaf98b37c340aa
SarahTomaz <Sarah_Tomaz_@Outlook.com>
2025-07-21 22:33:49 -0300

    Commit do projeto completo na branch main

--- Criaturas-Saltitantes-ref-recreate-project/src/main/java/org/example/Main.java
+++ Criaturas-Saltitantes-ref-recreate-project/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Inicia a interface gráfica da simulação de criaturas saltitantes
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new org.example.ui.MainFrame().setVisible(true);
+        });
     }
 }
